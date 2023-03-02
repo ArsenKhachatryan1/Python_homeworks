3652ef9f4fbba716b6e690faac04041fb79319a0
arsengeologist <arsen.khachatryan.91@gmail.com>
2023-03-02 16:39:12 +0400

    das26

---
+++ das26.py
@@ -0,0 +1,22 @@
+
+# strs = ["dog","racecar","car"]
+# strs.sort(key=len)
+# a = strs[0]
+# strs.remove(a)
+# b = ''
+# c = ''
+# d = ''
+# k = 0
+# while k != len(a):
+#     for i in strs:
+#         if a[k] == i[k]:
+#             b += i[k]
+#         else:
+#             c += a[k]
+#     k += 1
+# for i in a:
+#     if i not in c:
+#         d += i
+# print(d)
+
+
